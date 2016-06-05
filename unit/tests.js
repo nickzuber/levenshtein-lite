9536d5afec4ef0515095ebfce1a8c60c09f410f6
nickzuber <nickzoober@gmail.com>
2016-06-04 23:24:16 -0400

    More unit test

--- unit/tests.js
+++ unit/tests.js
@@ -8,31 +8,38 @@ Tape('Should return 3', function(t) {
   // Basic tests
   try {
     var distance = LevenshteinLite('kitten', 'sitting');
-    t.equal(distance, 3, 'Checking to see if Levenshtein distance was calculated properly.')
+    t.equal(distance, 3, 'Checking to see if Levenshtein distance was calculated properly.');
+    distance = LevenshteinLite('summertime', 'spring');
+    t.equal(distance, 7, 'Checking to see if Levenshtein distance was calculated properly.');
+    distance = LevenshteinLite('book', 'back');
+    t.equal(distance, 2, 'Checking to see if Levenshtein distance was calculated properly.');
+    distance = LevenshteinLite('crack', 'crouton');
+    t.equal(distance, 5, 'Checking to see if Levenshtein distance was calculated properly.');
+    distance = LevenshteinLite('hoopla', 'hadoop');
+    t.equal(distance, 4, 'Checking to see if Levenshtein distance was calculated properly.');
+    distance = LevenshteinLite('playground', 'playgoudn');
+    t.equal(distance, 3, 'Checking to see if Levenshtein distance was calculated properly.');
+    distance = LevenshteinLite('granola', 'grandma');
+    t.equal(distance, 2, 'Checking to see if Levenshtein distance was calculated properly.');
+    distance = LevenshteinLite('levenshtein', 'meilenstein');
+    t.equal(distance, 4, 'Checking to see if Levenshtein distance was calculated properly.');
   } catch(e) {
-    t.fail('LevenshteinLite has failed ungracefully: ' + e.message)
-  }
-
-  try {
-    var distance = LevenshteinLite('summertime', 'spring');
-    t.equal(distance, 7, 'Checking to see if Levenshtein distance was calculated properly.')
-  } catch(e) {
-    t.fail('LevenshteinLite has failed ungracefully: ' + e.message)
+    t.fail('LevenshteinLite has failed ungracefully: ' + e.message);
   }
 
   // With distance cap
   try {
-    var distance = LevenshteinLite('summertime', 'spring', 3);
-    t.equal(distance, -1, 'Checking to see if Levenshtein distance exits early when cap is hit.')
   } catch(e) {
-    t.fail('LevenshteinLite has failed ungracefully: ' + e.message)
+    t.fail('LevenshteinLite has failed ungracefully: ' + e.message);
   }
 
   try {
     var distance = LevenshteinLite('kitten', 'sitting', 2);
-    t.equal(distance, -1, 'Checking to see if Levenshtein distance exits early when cap is hit.')
+    t.equal(distance, -1, 'Checking to see if Levenshtein distance exits early when cap is hit.');
+    distance = LevenshteinLite('summertime', 'spring', 3);
+    t.equal(distance, -1, 'Checking to see if Levenshtein distance exits early when cap is hit.');
   } catch(e) {
-    t.fail('LevenshteinLite has failed ungracefully: ' + e.message)
+    t.fail('LevenshteinLite has failed ungracefully: ' + e.message);
   }
 
   t.end();
